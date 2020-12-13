b1758b237c58a15d66165cf4461bfaaecbfe9058
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-12-13 17:02:19 -0600

    first components

---
+++ views/CrearCuenta.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const CrearCuenta = () => {
+    return (
+        <View>
+            <Text>Login</Text>
+        </View>
+    )
+}
+
+export default CrearCuenta;
+
+const styles = StyleSheet.create({})
