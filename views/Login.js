4ac92dbb47a0133611ee996265d71ef9b1d3baad
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-12-13 19:58:48 -0600

    Styles for the login

--- views/Login.js
+++ views/Login.js
@@ -1,39 +1,41 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import {Container, Button, Text, H1, Input, Form, Item, Toast} from 'native-base';
+import {StyleSheet, View} from 'react-native';
+import {
+  Container,
+  Button,
+  Text,
+  H1,
+  Input,
+  Form,
+  Item,
+  Toast,
+} from 'native-base';
+//Styles
+import globalStyles from '../styles/global';
 
 const Login = () => {
-    return (
-        <Container>
-            <View>
-                <H1>upTask</H1>
-                <Form>
-                    <Item inlineLabel last>
-                        <Input
-                            autoCompleteType="email"
-                            placeholder="Email"
-                        />
-                    </Item>
-                    <Item inlineLabel last>
-                        <Input
-                            secureTextEntry={true}
-                            placeholder="Password"
-                        />
-                    </Item>
-                    <Button
-                        square
-                        block
-                    >
-                        <Text>Iniciar Sesión</Text>
-                    </Button>
+  return (
+    <Container style={[globalStyles.contenedor, {backgroundColor: '#e84347'}]}>
+      <View style={globalStyles.contenido}>
+        <H1 style={globalStyles.titulo}>UpTask</H1>
+        <Form>
+          <Item inlineLabel last style={globalStyles.input}>
+            <Input autoCompleteType="email" placeholder="Email" />
+          </Item>
+          <Item inlineLabel last style={globalStyles.input}>
+            <Input secureTextEntry={true} placeholder="Password" />
+          </Item>
+          <Button square block style={globalStyles.boton}>
+            <Text style={globalStyles.botonTexto}>Iniciar Sesión</Text>
+          </Button>
 
-                    <Text>Crear Cuenta</Text>
-                </Form>
-            </View>
-        </Container>
-    )
-}
+          <Text style={globalStyles.enlace}>Crear Cuenta</Text>
+        </Form>
+      </View>
+    </Container>
+  );
+};
 
 export default Login;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
